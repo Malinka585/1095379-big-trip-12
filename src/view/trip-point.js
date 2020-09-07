1ef9b9e9a3c11ed4e3eaf3054636a1f1f920e926
Alina <59539395+Malinka585@users.noreply.github.com>
2020-09-07 22:26:47 +0500

    Поправляет код в задании  Личный проект: шаблонизируй это

--- src/view/trip-point.js
+++ src/view/trip-point.js
@@ -4,20 +4,11 @@ export const createTripPointTemplate = (wayPoint, index) => {
   const DAY_GAP = 1;
   const dayNumber = index + DAY_GAP;
 
-  const dateByDataTime = (date) => {
-    return `${date.toLocaleString(`en-US`, {year: `numeric`})}-${date.toLocaleString(`en-US`, {month: `2-digit`})}-${date.toLocaleString(`en-US`, {day: `2-digit`})}`;
-  };
-
-  const dayDate = (Date) => {
-    return Date.toLocaleString(`en-US`, {month: `short`, day: `numeric`});
-  };
-
-
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
         <span class="day__counter">${dayNumber}</span>
-        <time class="day__date" datetime="${dateByDataTime(currentDate)}">${dayDate(currentDate)}</time>
+        <time class="day__date" datetime="${currentDate}">${currentDate}</time>
       </div>
 
       <ul class="trip-events__list">
